9ee2839894d0165b7e50efb8d659447da6731681
Rodrigo Cesar Antunes <rodrigo.antunes@jmoraes.com.br>
2021-03-10 18:33:03 -0300

    update: Explicação exercicios

--- EntrevistaTesteSegundaFase/Program.cs
+++ EntrevistaTesteSegundaFase/Program.cs
@@ -12,8 +12,9 @@ namespace EntrevistaTesteSegundaFase
         static void Main(string[] args)
         {
             //Durante a realização dos Exercicios fique a vontate para criar pastas e classes
-            //Parte 1 - Crie um menu para que o valiador possa escolher 
-            //entre executar a parte 2 ou a parte 3 do exercicio apertando os numeros 2 e 3 no console
+            //Parte 1 - Crie um menu para que o usuario possa escolher apertando botões 1, 2 e 3
+            // 1 para sair do programa, 2 para o exercicio parte 2 e 3 para o exercicio parte 3
+
 
             ExercicioParte2();
 
@@ -23,20 +24,21 @@ namespace EntrevistaTesteSegundaFase
         private static void ExercicioParte2()
         {
             //1 - ordene os numeros de maneira decrescente e mostre no console o resultado.
-            var numeros = new List<int> { 18, 92, 43, 11, 90, 56, 87, 78, 11, 18, 56};
+            var numeros = new int[] { 18, 92, 43, 11, 90, 56, 87, 78, 11, 18, 56};
 
-            //2 - Com a lista ordenada remova os números repetidos e mostre no console o resultado.
+            //2 - Com o vetor ordenado remova os números repetidos e mostre no console o resultado.
 
         }
 
         private static void ExercicioParte3()
         {
             //1 - Extraia o texto do arquivo 'Texto Ipsum.txt' e atribua-o a variável texto
-            //Coloque aqui o caminho do arquivo Ipsum
+            //DICA: Coloque o caminho do arquivo Texto Ipsum na variavel caminhoArquivoIpsum
+            //DICA 2: Use a classe File para extrair o texto
             string caminhoArquivoIpsum = @"";
             string texto = default;
 
-            //2 - Mostre o texto no console
+            //2 - Mostre o texto do arquivo no console
 
             //3 - No meio do texto tem o valor {Referencia: 958220}.
             //    Extraia o valor 958220 direto do texto e atribua-o a variavel referencia.
@@ -45,7 +47,7 @@ namespace EntrevistaTesteSegundaFase
 
             //Salve o texto da variável relatório em um novo arquivo de texto na pasta documentos do windows
             // O nome do arquivo de texto será: relatorio_referencia_958220.txt
-            //Dica: use a variavel caminhoPastaDocumento para criar o caminho de salvamento do arquivo
+            //Dica: use a variavel caminhoPastaDocumento, nela está o caminho para a pasta documentos do windows
             var relatorioService = new RelatorioService();
             var relatorio = relatorioService.GetTexto(referencia);
             var caminhoPastaDocumento = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
